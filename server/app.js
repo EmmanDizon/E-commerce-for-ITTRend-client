36db7ff62ff1cd00f932e45e13c98f545b3553d5
Emman-Jay-Dizon <emman.jay_dizon@questronix.com.ph>
2022-04-03 20:30:49 +0800

    add morgan for logging http request for tracking errors and cors for allowing for resources sharing

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 require("./database")();
 
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 
@@ -12,6 +14,8 @@ const errorRouteHandling = require("./middlewares/error_routes");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/api/products", products);
 app.use(errorHandling);
