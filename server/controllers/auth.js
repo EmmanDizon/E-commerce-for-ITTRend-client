7bdf7bae663655f1d4bb1de4f47528b59f3c552d
Emman-Jay-Dizon <emman.jay_dizon@questronix.com.ph>
2022-04-05 01:19:49 +0800

    remove validation for empty email and password. the front end already validates it

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,9 +15,6 @@ exports.registerUser = TryCatch(async (req, res, next) => {
 exports.loginUser = TryCatch(async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password)
-    return next(new ErrorHandler("Please enter email and password", 400));
-
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) return next(new ErrorHandler("Invalid email or password", 401));
