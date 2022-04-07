6abb1e08f2cc793a1ac01cb682921b290c097bc5
Emman-Jay-Dizon <emman.jay_dizon@questronix.com.ph>
2022-04-08 01:40:21 +0800

    remove unecessary line of codes

--- client/src/functions/rating.js
+++ client/src/functions/rating.js
@@ -5,19 +5,14 @@ export const showAverage = (p) => {
   let ratingsArray = p && p.reviews;
   let total = [];
   let length = ratingsArray.length;
-  // console.log("length", length);
 
   ratingsArray.map((r) => total.push(r.ratings));
   let totalReduced = total.reduce((p, n) => p + n, 0);
-  // console.log("totalReduced", totalReduced);
 
   let highest = length * 5;
-  // console.log("highest", highest);
 
   let result = (totalReduced * 5) / highest;
-  // console.log("result", result);
 
-  console.log("XXXXXXXXX", result);
   return (
     <div className="text-center pt-1 pb-3">
       <span>
@@ -27,7 +22,7 @@ export const showAverage = (p) => {
           starRatedColor="blue"
           rating={result}
           editing={false}
-        />{" "}
+        />
         ({p.reviews.length})
       </span>
     </div>
